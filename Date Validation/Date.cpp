856871b4fad0a07a8c4b07b560981d13b852aa87
Piero <Piero@ERICK>
2022-09-23 19:40:49 -0500

    Agregar archivos de proyecto.

---
+++ Date Validation/Date.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+#pragma once
+class Date
+{
+private:
+	int day;
+	int month;
+	int year;
+	
+public:
+	Date(int _day, int _month int _year);
+	void leer;
+	void january;
+	void bisciest;
+	Date(int _day, int _month, int _year);
+};
+
+
+Date::Date(int _day, int _month, int _year) {
+	year = _year;
+	day = _day;
+	month = _month;
+
+}
+
+
+int main() {
+	
+	cout << "Enter a date" << endl;
+
+}
+
